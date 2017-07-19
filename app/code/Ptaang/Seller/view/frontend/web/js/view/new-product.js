7560e406bd2f740e71414c80f1d022a400de9e98
israel <israel@wagento.com>
2017-07-18 21:57:24 -0400

    Edit and create product implement

--- app/code/Ptaang/Seller/view/frontend/web/js/view/new-product.js
+++ app/code/Ptaang/Seller/view/frontend/web/js/view/new-product.js
@@ -14,7 +14,7 @@ define([
 
 ], function (ko,Component, $ , $t, newProduct, changeAttributeSet, createProduct, searchSku) {
     'use strict';
-
+    productId: productId;
     return Component.extend({
         defaults: {
             categoryList: [],
@@ -93,10 +93,20 @@ define([
                     });
                 }else if(expExtensionAttr.exec(attributeCode)) {
                     if(attributeCode.replace(expReplaceExt, "") == "qty"){
-                        extensionAttributes = {
-                            stockItem : {
-                                qty: entry.value,
-                                isInStock: true
+                        if(productId == 0){
+                            extensionAttributes = {
+                                stockItem : {
+                                    qty: entry.value,
+                                    isInStock: true
+                                }
+                            }
+                        }else{ //Updating the product
+                            extensionAttributes = {
+                                stockItem : {
+                                    productId: productId,
+                                    qty: entry.value,
+                                    isInStock: true
+                                }
                             }
                         }
                     }
@@ -130,18 +140,33 @@ define([
             var images = newProduct.imagesFile(), entries = [];
             for(var i in images){
                 var image = images[i], imageType = image.type;
-
-                entries[i] = {
-                    position: i,
-                    media_type: 'image',
-                    disabled: false,
-                    types: i == 1 ? ['image','small_image','thumbnail'] : [],
-                    content: {
-                        type: imageType,
-                        name: image.name,
-                        base64_encoded_data: image.src.replace("data:"+ imageType+";base64,","")
+                if(image.entry_id == 0){
+                    entries[i] = {
+                        position: i,
+                        media_type: 'image',
+                        disabled: false,
+                        types: i == 1 ? ['image','small_image','thumbnail'] : [],
+                        content: {
+                            type: imageType,
+                            name: image.name,
+                            base64_encoded_data: image.src.replace("data:"+ imageType+";base64,","")
+                        }
+                    }
+                }else{
+                    entries[i] = {
+                        id: image.entry_id,
+                        position: i,
+                        media_type: 'image',
+                        disabled: false,
+                        types: i == 1 ? ['image','small_image','thumbnail'] : [],
+                        file:  image.path,
+                        content: {
+                            type: imageType,
+                            name: image.name
+                        }
                     }
                 }
+
             }
             return entries;
         },
