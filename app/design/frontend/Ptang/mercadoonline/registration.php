45e3e0df553015a8172e4b383c55241dd5c10a6d
Rulo <rencinas@waskiri.com>
2017-07-24 18:49:22 -0400

    add new theme

---
+++ app/design/frontend/Ptang/mercadoonline/registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by Raul Encinas.
+ * User: Rulo
+ * Date: 3/11/17
+ * Time: 12:37 PM
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Ptang/mercadoonline',
+    __DIR__
+);
